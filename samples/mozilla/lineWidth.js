ffaac4cea540ca7009729dcf268d1a66f2115042
Ryan Tenney <Ryan Tenney@15cfa1a4-d5a4-44d2-8e29-f8dcda5bb17e>
2011-07-13 06:28:31 +0000

    Some more samples from Mozilla.
    git-svn-id: svn+ssh://multivac.martellotech.com/svn/MarWatch/personal/rtenney/rhino-canvas@6458 15cfa1a4-d5a4-44d2-8e29-f8dcda5bb17e

---
+++ samples/mozilla/lineWidth.js
@@ -0,0 +1,8 @@
+var ctx = document.getElementById('canvas').getContext('2d');
+for (var i = 0; i < 10; i++){
+	ctx.lineWidth = 1+i;
+	ctx.beginPath();
+	ctx.moveTo(5+i*14,5);
+	ctx.lineTo(5+i*14,140);
+	ctx.stroke();
+}
